392495bc69781a76f5de30c2aa9fab9bc54e64d4
Niveditha VM <niveditha.vm13@gmail.com>
2022-02-08 21:52:30 +0530

    Project Initiated

---
+++ todo-list/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     To-Do Project
+    </div>
+  );
+}
+
+export default App;
